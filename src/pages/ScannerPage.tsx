6f674f6eaa9637aabb1597143696ce3d4fabe9ee
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-07-04 14:59:21 +0530

    feat: mount scanner utility center

--- src/pages/ScannerPage.tsx
+++ src/pages/ScannerPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChartinkScannerHeaderLayer from '../components/ChartinkScannerHeaderLayer';
+import ChartinkScannerUtilityPanel from '../components/ChartinkScannerUtilityPanel';
 import StockProScannerFeatureLayer from '../components/StockProScannerFeatureLayer';
 import ChartinkStyleScanner from '../components/ChartinkStyleScanner';
 import { useDashboard } from '../components/Layout';
@@ -10,6 +11,7 @@ export default function ScannerPage() {
   return (
     <div className="lg:col-span-12 flex flex-col gap-6" id="chartink_screener_view">
       <ChartinkScannerHeaderLayer />
+      <ChartinkScannerUtilityPanel />
       <StockProScannerFeatureLayer stocks={stockData?.length ? stockData : stocks} />
       <ChartinkStyleScanner
         stocks={stocks}
